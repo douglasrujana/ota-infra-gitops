59897a2ee0ef2508f6593e0ef5450766e58b08c8
douglasrujana <drrclabx@gmail.com>
2025-02-02 21:32:39 -0500

    Update AWS-arn

--- cdk/stacks/gitops-iam-role.py
+++ cdk/stacks/gitops-iam-role.py
@@ -1,25 +1,22 @@
 # Crear un IAM Role para GitHub Actions (Automáticamente con CDK)
 
-from aws_cdk import (
-    aws_iam as iam,
-    core
-)
+from aws_cdk import App, Stack
+from aws_cdk import aws_iam as iam
 
-class GitOpsIAMRole(core.Stack):
-    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+class GitOpsIAMRole(Stack):
+    def __init__(self, scope: App, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
         # Crear el rol para GitHub Actions
         github_actions_role = iam.Role(
             self, "GitHubActionsRole",
             assumed_by=iam.FederatedPrincipal(
-                "arn:aws:iam::aws:policy/AmazonS3FullAccess",
+                federated="arn:aws:iam::aws:oidc-provider/token.actions.githubusercontent.com",
                 conditions={
                     "StringEquals": {
                         "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
                     }
-                },
-                action="sts:AssumeRoleWithWebIdentity"
+                }
             ),
             description="Role for GitHub Actions to deploy with CDK"
         )
@@ -36,3 +33,7 @@ class GitOpsIAMRole(core.Stack):
             ],
             resources=["*"]
         ))
+
+app = App()
+GitOpsIAMRole(app, "GitOpsIAMRole")
+app.synth()
